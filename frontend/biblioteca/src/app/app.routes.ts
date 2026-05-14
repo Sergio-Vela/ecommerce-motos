202c4abcd2dbec15044d9343152cba459f30010a
Diego Méndez <202102281@estudiante.udv.edu.gt>
2026-05-13 19:51:13 -0600

    QA actualización de rutas para inicio de Sprint 2

--- frontend/biblioteca/src/app/app.routes.ts
+++ frontend/biblioteca/src/app/app.routes.ts
@@ -1,1 +1,12 @@
 import { Routes } from '@angular/router';
+// Importamos usando la ruta y nombres que confirmamos en el servidor
+import { Login } from './components/login/login'; 
+import { Register } from './components/register/register';
+import { Catalogo } from './components/catalogo/catalogo';
+
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'catalogo', component: Catalogo },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
